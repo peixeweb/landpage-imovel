58772ef922ea5248f7cb63afa2374525b8b7694c
Peixeweb <140914200+peixeweb@users.noreply.github.com>
2026-08-17 17:27:46 -0300

    Configurar GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/landpage-imovel/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
